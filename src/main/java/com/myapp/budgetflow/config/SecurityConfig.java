ea3708b98cc2448cdf083254e2bc2ca8ce8eb089
fkauanGIT <kauanuniversity@gmail.com>
2025-03-31 10:58:11 -0300

    feat: I created the passwordencoder beans and the email messaging bean

---
+++ src/main/java/com/myapp/budgetflow/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.myapp.budgetflow.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Properties;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com"); // Alterar conforme seu provedor
+        mailSender.setPort(587);
+        mailSender.setUsername("seuemail@gmail.com"); // Seu e-mail
+        mailSender.setPassword("suasenha"); // Sua senha ou App Password
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
