93a3a7cc2c8a8e16c48a89935f03fcf22a7ae038
fkauanGIT <kauanuniversity@gmail.com>
2025-04-01 14:46:40 -0300

    feat: add email configuration for notifications using JavaMailSender

---
+++ src/main/java/com/myapp/budgetflow/config/NotificationConfig.java
@@ -0,0 +1,29 @@
+package com.myapp.budgetflow.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class NotificationConfig {
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com"); // Alterar conforme seu provedor
+        mailSender.setPort(587);
+        mailSender.setUsername("seuemail@gmail.com"); // Seu e-mail
+        mailSender.setPassword("suasenha"); // Sua senha ou App Password
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
