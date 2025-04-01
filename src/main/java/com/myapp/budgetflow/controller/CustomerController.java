b7ca472d8c4edb22e715137fafcc17fa899f33e3
fkauanGIT <kauanuniversity@gmail.com>
2025-04-01 14:52:19 -0300

    feat: Implement customer registration and fetch user details after login

---
+++ src/main/java/com/myapp/budgetflow/controller/CustomerController.java
@@ -0,0 +1,52 @@
+package com.myapp.budgetflow.controller;
+
+import com.myapp.budgetflow.model.Customer;
+import com.myapp.budgetflow.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/customers")
+public class CustomerController {
+
+    private final CustomerRepository customerRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final Environment env;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody Customer customer) {
+        try {
+            String hashPwd = passwordEncoder.encode(customer.getPwd());
+            customer.setPwd(hashPwd);
+            customer.setCreateDt(LocalDateTime.now());
+            Customer savedCustomer = customerRepository.save(customer);
+
+            if (savedCustomer.getId() > 0) {
+                return ResponseEntity.status(HttpStatus.CREATED).
+                        body("Given user details are successfully registered");
+            } else {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).
+                        body("User registration failed");
+            }
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).
+                    body("An exception occurred: " + ex.getMessage());
+        }
+    }
+
+    @RequestMapping("/user")
+    public Customer getUserDetailsAfterLogin(Authentication authentication) {
+        Optional<Customer> optionalCustomer = customerRepository.findByEmail(authentication.getName());
+        return optionalCustomer.orElse(null);
+    }
+}
